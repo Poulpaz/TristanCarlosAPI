2c9b344f24147230369af632f7f16ad79a594a6d
Carlos1404 <carlos.chastagnier@gmail.com>
2018-12-29 21:31:27 +0100

    updateUser has been updated and writted

--- controllers/userCardController.js
+++ controllers/userCardController.js
@@ -64,10 +64,10 @@ exports.deleteUserCard = function (req, res, next) {
 }
 
 exports.updateUserCard = function (req, res, next) {
-    var idUserCard = req.body.uc;
+    var idOldUser = req.body.olduser;
     var user_idUser = req.body.user;
     var card_idCard = req.body.card;
-    connectionOnline.query("UPDATE usercard SET user_iduser='" + user_idUser + "', card_idcard='" + card_idCard + "' WHERE idusercard=" + idUserCard + "", function (err, result, fields) {
+    connectionOnline.query("UPDATE usercard SET user_iduser='" + user_idUser + "', card_idcard='" + card_idCard + "' WHERE user_iduser=" + idOldUser + "", function (err, result, fields) {
         if (err) {
             throw err;
         } else {
@@ -78,9 +78,6 @@ exports.updateUserCard = function (req, res, next) {
 
 exports.getAllUserCardWithToken = function (req, res, next) {
     var token = req.params.token;
-
-    var option = "https://api.elderscrollslegends.io/v1/cards/";
-    var data = "";
 
     connectionOnline.query("SELECT card_idcard FROM usercard, user WHERE usercard.user_iduser=user.idUser AND user.token=" + token + "", function (err, result, fields) {
         if (err) {
@@ -93,61 +90,4 @@ exports.getAllUserCardWithToken = function (req, res, next) {
         }
         res.json(result);
     });
-
-    Array.prototype.forEach.call(res.card_idcard, idcard => {
-        console.log(idcard);
-    })
-
-
-    res.forEach(card => {
-        var request = https.get(option + card.card_idcard, (result) => {
-            result.on('data', (d) => {
-                data += d;
-            });
-            result.on('end', function () {
-                var card = JSON.parse(data);
-                res.json(card);
-            });
-        });
-        request.on('error', (e) => {
-            console.error(e);
-        });
-        request.end();
-    });
-
-
-    /*
-        connectionOnline.query("SELECT card_idcard FROM usercard, user WHERE usercard.user_iduser=user.idUser AND user.token=" + token
-            + "", function (err, result, fields) {
-                if (err) {
-                    throw err;
-                } else {
-                    Object.keys(result).forEach(function (key) {
-                        var row = result[key];
-                        console.log(row.card_idcard);
-    
-                        var option = "https://api.elderscrollslegends.io/v1/cards/" + row.card_idcard;
-                        var data = "";
-                    
-                        var request = https.get(option, (result) => {
-                            result.on('data', (d) => {
-                                data += d;
-                            });
-                            result.on('end', function () {
-                                //var card = JSON.parse(data);
-                                cards.push(JSON.parse(data))
-                                //console.log(JSON.parse(data));
-    
-                                //res.json(card);
-                            });
-                        });
-                        request.on('error', (e) => {
-                            console.error(e);
-                        });
-                        request.end();
-                    });
-                    console.log(cards);
-                    res.json(cards);
-                }
-            }); */
 }
