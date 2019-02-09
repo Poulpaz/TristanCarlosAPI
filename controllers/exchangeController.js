a3c4eb531c4529f6b9d3d5a829aa1f0a38aa7960
Poulpaz <letristandu39@hotmail.fr>
2019-02-09 11:21:44 +0100

    [FIX] GetExchange.

--- controllers/exchangeController.js
+++ controllers/exchangeController.js
@@ -8,7 +8,7 @@ var connectionOnline = mysql.createConnection({
 });
 
 exports.listExchange = function (req, res, next) {
-    connectionOnline.query("SELECT * FROM exchange, user, usercard WHERE exchange.user_idUser = user.idUser AND exchange.user_idOtherUser = user.idUser AND exchange.userCard_idUserCard = usercard.idusercard AND exchange.userCard_idUserCard = usercard.idusercard", function (err, result, fields) {
+    connectionOnline.query("SELECT * FROM exchange WHERE 1", function (err, result, fields) {
         if (err) { throw err; }
         else { res.json(result); }
     });
